2b8cc2fe7e291b17d0efcf70da8e3a130e088aee
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2021-08-28 18:56:42 -0300

    Add files via upload

---
+++ parte1/main/java/tdd/Dollar.java
@@ -0,0 +1,15 @@
+//package tdd;
+//
+//public class Dollar extends Money {
+//    public Dollar(int amount, String currency) {
+//        super(amount, currency);
+//    }
+//
+//    public static Money dollar(int amount){
+//        return new Money(amount, "USD");
+//    }
+//
+//    public String currency() {
+//        return currency;
+//    }
+//}
---
+++ parte1/main/java/tdd/Franc.java
@@ -0,0 +1,15 @@
+//package tdd;
+//
+//public class Franc extends Money {
+//    public Franc(int amount, String currency) {
+//        super(amount, currency);
+//    }
+//
+//    public static Money franc(int amount){
+//        return new Money(amount, "CHF");
+//    }
+//
+//    public String currency() {
+//        return currency;
+//    }
+//}
